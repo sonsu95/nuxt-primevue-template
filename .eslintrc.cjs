0fba149bdf403a6d3f0883c9e32b47889e33127c
Sungsu <127682098+sonsu95@users.noreply.github.com>
2024-05-01 14:51:47 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,88 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    'airbnb-base',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    '@nuxt/eslint-config',
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/newline-after-import': 'off',
+    'no-param-reassign': 'off',
+    'no-void': 'off',
+    'no-nested-ternary': 'off',
+    'max-classes-per-file': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'import/first': 'off',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'import/named': 'off',
+    'prefer-promise-reject-errors': 'off',
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-unused-vars': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'prefer-destructuring': [
+      'error',
+      {
+        array: false,
+        object: true,
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'vue/no-setup-props-destructure': 'off',
+    'max-len': [
+      'warn',
+      100,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# Nuxt 3 Template
+
+## Nuxt Upgrade
+```bash
+bunx nuxi upgrade
+```
+## How to use
+```bash
+# Requires docker-desktop
+docker-compose up --build -d # Run the container after building the image
+docker-compose up # Run the container
+docker-compose down # Terminating a Container
+```
